8151df4b3fddb9949f63a420fb83efeffe94348b
sudhan regmi <xenonrazels@gmail.com>
2021-06-08 09:16:14 +0545

    d

--- englishDictionary/settings.py
+++ englishDictionary/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'englishDictionary.urls'
@@ -119,11 +119,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-# STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = 'staticfiles'
 
-STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- englishDictionary/wsgi.py
+++ englishDictionary/wsgi.py
@@ -12,4 +12,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'englishDictionary.settings')
-application = get_wsgi_application()
+
+application = Cling(get_wsgi_application())
