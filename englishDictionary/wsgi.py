68c019ab2046c8be2bd5b32f1354e8e9b2c1330d
sudhan regmi <xenonrazels@gmail.com>
2021-06-08 07:46:15 +0545

    refreeze

--- englishDictionary/wsgi.py
+++ englishDictionary/wsgi.py
@@ -10,6 +10,5 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'englishDictionary.settings')
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
